c2a1ac1e84f778a2dbbb5af07cbba0fbbb58ffd1
Agustin Cejas <agus.e.cejas@gmail.com>
2024-10-03 18:37:20 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,89 @@
+//EJERCICIO 1
+// function numeroParImpar(num) {
+//     if (num %2 === 0) {
+//         console.log(`El numero ${num} es par`);
+//     } else {
+//         console.log(`El numero ${num} es impar`);
+//     }
+// }
+
+// numeroParImpar(29)
+
+//EJERCICIO 2
+// function numeroMayor(num1, num2) {
+//     if (num1 > num2) {
+//         console.log(`El numero ${num1} es mayor que ${num2}`);
+//     }
+//     else if (num1 === num2) {
+//         console.log(`Los numeros son iguales`);
+//     }
+//     else {
+//         console.log(`El numero ${num2} es mayor que ${num1}`);
+//     }
+// }
+
+// numeroMayor(24,32)
+
+//EJERCICIO 3
+// function multiploCinco(num) {
+//     if (num % 5 === 0) {
+//         console.log(`El numero ${num} es multiplo de 5`);
+//     }
+//     else {
+//         console.log(`El numero ${num} no es multiplo de 5`);
+//     }
+// }
+
+// multiploCinco(3)
+
+//EJERCICIO 4
+// function imprimirNumeros(num) {
+//     for (let i = 0; i <= num; i++) {
+//         console.log(i)
+//     }
+// }
+
+// imprimirNumeros(30)
+
+//EJERCICIO 5
+// function palabraRepetida(palabra, veces) {
+//     for (let index = 0; index <= veces - 1; index++) {
+//         console.log(palabra);
+//     }
+// }
+
+// palabraRepetida("Agustin", 5)
+
+//EJERCICIO 6
+// function valoresArray(valores) {
+//     for (let index = 0; index < valores.length; index++) {
+//         console.log(valores[index]);
+//     }
+// }
+
+// valoresArray([1,2,3,4,5])
+
+//EJERCICIO 7
+// function arrayDeDiez(numeros) {
+//     for (let index = 0; index < numeros.length; index++) {
+//         if (index === 4) {
+//             continue
+//         }
+//         console.log(numeros[index]);
+//     }
+// }
+
+// arrayDeDiez([1,2,3,4,5,6,7,8,9,10])
+
+//EJERCICIO 8
+// function multiplicacionArray(arrayDeNumeros, numeroMultiplicador) {
+//     for (let index = 0; index < arrayDeNumeros.length; index++) {
+//         console.log(arrayDeNumeros[index] * numeroMultiplicador);
+//     }
+// }
+
+// multiplicacionArray([1,2,3,4,5], 7)
+
+
+
+
